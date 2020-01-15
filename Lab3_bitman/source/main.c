9864f7964dde65caaa3fc650b806610b8d042b46
Valeria Garibaldi <vgari001@wch136-33.cs.ucr.edu>
2020-01-15 13:09:09 -0800

    Added files

---
+++ Lab3_bitman/source/main.c
@@ -0,0 +1,56 @@
+/*	Author: vgari001
+ *  Partner(s) Name: Victoria Albezer
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    
+
+DDRA = 0x00; PORTA =0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0xFF ; PORTC = 0x00;
+
+        unsigned char PENA0  = 0x00;
+	unsigned char PENB0 = 0x00;	
+	unsigned char i=0x00;
+	unsigned char counter = 0x00;
+	unsigned char tempA = 0x00;
+	unsigned char tempB = 0x00;
+
+    
+    while (1) {
+		PENA0 = PINA & 0x01;
+       PENB0 = PINA & 0x01;
+	counter =  0;
+	tempA = PENA0;
+	tempB = PENB0;
+	for(i=0; i<8; ++i){
+		
+		if(tempA  == 0x01){
+
+			counter++;
+		
+		}
+	tempA =	PENA0>>1;
+
+		if(tempB == 0x01){
+		counter++;
+		}
+
+	tempB = PENB0>>1;
+
+    }
+      PORTC= counter;
+
+    }
+    return 1;
+}
